f0aa2173d91775cb2b26f766846cf9a1340803f5
serizelle <116939260+serizelle@users.noreply.github.com>
2023-04-07 16:17:42 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,11 +9,81 @@ closeButtonEl.addEventListener('click', function (e) {
     modalEl.classList.toggle('hidden');
 });
 
-//
+const modalEl2 = document.querySelector('.modal2');
+const openButtonEl2 = document.querySelector('.button-open2');
+const closeButtonEl2 = document.querySelector('.button-close2');
 
-const burgerEl = document.querySelector('.logo');
+openButtonEl2.addEventListener('click', function (e) {
+    modalEl2.classList.toggle('hidden');
+});
+closeButtonEl2.addEventListener('click', function (e) {
+    modalEl2.classList.toggle('hidden');
+});
+
+//BURGER
+
+const burgerEl = document.querySelector('.logo__mobile');
 const menuEl = document.querySelector('.menu__burger');
 
 burgerEl.addEventListener('click', function (e) {
     menuEl.classList.toggle('menu_show');
 });
+
+//about me slider
+const panels = document.querySelectorAll('.panel');
+
+ panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active');
+    })
+ })
+
+ function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+ }
+
+ //CAROUSEL
+ const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0
+
+let interval = setInterval(run, 2000)
+
+function run() {
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > img.length - 1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 400}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+
+rightBtn.addEventListener('click', () => {
+    idx++
+    changeImage()
+    resetInterval()
+})
+
+leftBtn.addEventListener('click', () => {
+    idx--
+    changeImage()
+    resetInterval()
+})
